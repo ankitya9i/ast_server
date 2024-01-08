6a3f462def0042b8bdfb9e0e339e6d6273d117a8
ankitya9i <aktyagi18052002@gmail.com>
2024-01-08 22:40:35 +0530

    first commit

---
+++ src/database/database.module.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+// database.module.ts
+
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { databaseProviders } from './database.provider';
+
+@Module({
+  imports: [MongooseModule.forRoot(process.env.DATABASE_URI)],
+  providers: [...databaseProviders],
+  exports: [MongooseModule],
+   // Export MongooseModule for use in other modules
+})
+
+export class DatabaseModule {}
